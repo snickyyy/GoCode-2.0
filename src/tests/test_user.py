2e9c18e44268a09d379e23941ddbb6ac79ae4f6d
snickyyy <leon12345671234@gmail.com>
2024-12-17 00:42:54 +0100

    change testing system for github actions

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,22 +1,21 @@
-import httpx
+from starlette.testclient import TestClient
+from main import app
 
+client = TestClient(app)
 
 def test_register_user():
-    timeout = httpx.Timeout(10.0)
-    url = "http://127.0.0.1:8000/accounts/auth/register"
-    data = {"username": "testuser2", "email": "testuser2@example.com", "password": "test123"}
+    url = "/accounts/auth/register"
+    data = {"username": "testuser", "email": "testuser@example.com", "password": "test123"}
 
-    with httpx.Client(timeout=timeout) as client:
-        response = client.post(url, json=data)
-        assert response.status_code == 200
-        assert response.json()["detail"] == "an email has been sent"
+    response = client.post(url, json=data)
+    assert response.status_code == 200
+    assert response.json()["detail"] == "an email has been sent"
 
-        data.pop("password")
-        without_password = client.post(url, json=data)
-        assert without_password.status_code == 422
+    data_without_password = data.copy()
+    data_without_password.pop("password")
+    response = client.post(url, json=data_without_password)
+    assert response.status_code == 422
 
-    data = {"username": "testuser2", "email": "testuser2@example.com", "password": "test123"}
-    with httpx.Client(timeout=timeout) as client:
-        response = client.post(url, json=data)
-        assert response.status_code == 409
-        assert response.json()["detail"] == "This username or email already exists"
+    response = client.post(url, json=data)
+    assert response.status_code == 409
+    assert response.json()["detail"] == "This username or email already exists"
