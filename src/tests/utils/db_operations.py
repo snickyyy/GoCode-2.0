50b51a7e73134e5f94da5708700168fcaf26d43d
snickyyy <leon12345671234@gmail.com>
2025-01-07 21:55:04 +0100

    add some tests for problems

---
+++ src/tests/utils/db_operations.py
@@ -0,0 +1,30 @@
+from faker import Faker
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.problems.models import Test, Task, Category
+
+
+async def create_tests(count, session: AsyncSession):
+    f = Faker("EN")
+    test_for_task = [Test(path=f.file_path()) for i in range(count)]
+    session.add_all(test_for_task)
+    await session.commit()
+    return test_for_task
+
+async def create_tasks(count, session: AsyncSession):
+    f = Faker("EN")
+    stmt = select(Test.id).limit(1)
+    execute = await session.execute(stmt)
+    test_id = execute.scalar()
+    tasks = [Task(title=f.name_male(), description=f.text(10), difficulty=1, category_id=1, test_id=test_id, constraints=f.text(10), image=f.image_url()) for i in range(count)]
+    session.add_all(tasks)
+    await session.commit()
+    return tasks
+
+async def create_category(count, session: AsyncSession):
+    f = Faker("EN")
+    categories = [Category(name=f.catch_phrase()) for i in range(count)]
+    session.add_all(categories)
+    await session.commit()
+    return categories
